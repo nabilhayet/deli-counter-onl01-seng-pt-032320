d1263d2e38e842719b48453d9446c0c3a0c1258e
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-31 00:00:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,6 @@ if counter==0
   puts "The line is currently empty."
 else
 katz_deli.each_with_index { |val,index| message << "#{index+1}. #{val}" }
-message.each do |value|
 puts "The line is currently: " + message.join(" ")
 end
 end
